4e6a0690cecbaf8eef626e11e23fc015b56309e6
ola <o.bukhal@gmail.com>
2021-12-10 12:36:46 +0300

    feat: add adaptive for all resolution

--- src/components/MoviesContainer/MoviesTopBar/MoviesTopBar.tsx
+++ src/components/MoviesContainer/MoviesTopBar/MoviesTopBar.tsx
@@ -18,11 +18,10 @@ const MoviesTopBar: FC<MyProps> = ({foundMovie}) => {
 
     return (
         <div className={style.MovieResultsRow}>
-            <p>{foundMovie} movies found</p>
+            <p className={style.MovieFound}>{foundMovie} movies found</p>
             <div className={style.MovieSortBtn}>
                 Sort by 
                 {Object.values(MovieFilter).map(item => 
-                    // <Button key={item} name={item} handleClick={() => console.log({item})}/>
                     <Checkbox 
                         key={item} 
                         name={item} 
